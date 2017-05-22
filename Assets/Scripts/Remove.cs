a71d4bc1cfa276e8bee24a1225becd5fd0a2f536
Christopher Borger <Christopher.Borger@comhem.com>
2017-05-22 12:47:13 +0200

    key

---
+++ Assets/Scripts/Remove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Remove : MonoBehaviour
+{
+    private Rigidbody body;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Click");
+        this.body.AddForce(new Vector3(150, 0, 100));
+    }
+}
